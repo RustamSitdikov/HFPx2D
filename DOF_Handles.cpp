baa07a332e3e4edd6f1e189f1c40cb40b8f1ae6d
Federico Ciardo <federico.ciardo@epfl.ch>
2017-01-27 16:53:37 +0100

    Change of type of function

--- DOF_Handles.cpp
+++ DOF_Handles.cpp
@@ -2,37 +2,39 @@
 // HFPx2D project.
 //
 // Created by Brice Lecampion on 03.01.17.
-// Copyright (c) ECOLE POLYTECHNIQUE FEDERALE DE LAUSANNE, Switzerland, Geo-Energy Laboratory, 2016-2017.  All rights reserved.
+// Copyright (c) ECOLE POLYTECHNIQUE FEDERALE DE LAUSANNE, Switzerland,
+// Geo-Energy Laboratory, 2016-2017.  All rights reserved.
 // See the LICENSE.TXT file for more details.
 //
 //
 
+// Inclusion from Inside Loop library
 #include <il/Array2D.h>
 
+namespace hfp {
 
 //  FUNCTION TO CREATE A DOF HANDLE
-void dofhandle_DG2D(il::Array2D<int> &dofhandle, int dof_dim, int nelts, int p) {
-// function creating a matrix of dof handle - for a piece-wise linear variation per element (Discontinous Galerkin type)
-// on a 1d Mesh object for the case of dof_dim Degrees of Freedoms per node
-// format of the handle : number of elements \times (p+1)*dof_dim
-// dof_dim :: number of dof per node
-// nelts :: number of elements in the mesh
-// p :: interpolation order inside each element
-// needs to be generalized ....
+il::Array2D<int> dofhandle_DG2D(const int dof_dim, const int Nelts,
+                                const int p) {
+  // function creating a matrix of dof handle - for a piece-wise linear
+  // variation per element (Discontinous Galerkin type)
+  // on a 1d Mesh object for the case of dof_dim Degrees of Freedoms per node
+  // format of the handle : number of elements \times (p+1)*dof_dim
+  // dof_dim :: number of dof per node
+  // Nelts :: number of elements in the mesh
+  // p :: interpolation order inside each element
 
-  int ndof = nelts*(p+1)*dof_dim;
+  il::Array2D<int> Dof{Nelts, 2 * dof_dim, 0};
 
   int j;
 
-  for (int i = 0; i < nelts; ++i) {
-    j = i*dof_dim*(p+1) ;
-    for (int k=0; k<dof_dim*(p+1);++k) {
-      dofhandle(i, k) = j + k;
+  for (int i = 0; i < Nelts; ++i) {
+    j = i * dof_dim * (p + 1);
+    for (int k = 0; k < dof_dim * (p + 1); ++k) {
+      Dof(i, k) = j + k;
     }
   }
 
+  return Dof;
 }
-
-
-
-
+}
--- DOF_Handles.h
+++ DOF_Handles.h
@@ -10,8 +10,14 @@
 #ifndef HFPX2D_DOF_HANDLES_H
 #define HFPX2D_DOF_HANDLES_H
 
+// Inclusion from Inside Loop library
 #include <il/Array2D.h>
 
-void dofhandle_DG2D(il::Array2D<int> &dofhandle, int dof_dim, int nelts, int p);
+namespace hfp {
+
+    il::Array2D<int>
+    dofhandle_DG2D(const int dof_dim, const int Nelts, const int p);
+
+}
 
 #endif //HFPX2D_DOF_HANDLES_H
