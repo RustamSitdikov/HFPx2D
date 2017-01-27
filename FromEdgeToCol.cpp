cadf9d3e2e872cdfa2490d17af57377d23e49189
Federico Ciardo <federico.ciardo@epfl.ch>
2017-01-27 16:52:29 +0100

    FromEdgeToCol for EITHER opening OR shear

--- FromEdgeToCol.cpp
+++ FromEdgeToCol.cpp
@@ -2,61 +2,70 @@
 // This file is part of HFPx2D.
 //
 // Created by Federico Ciardo on 11.01.17.
-// Copyright (c) ECOLE POLYTECHNIQUE FEDERALE DE LAUSANNE, Switzerland, Geo-Energy Laboratory, 2016-2017.  All rights reserved.
-// See the LICENSE.TXT file for more details. 
+// Copyright (c) ECOLE POLYTECHNIQUE FEDERALE DE LAUSANNE, Switzerland,
+// Geo-Energy Laboratory, 2016-2017.  All rights reserved.
+// See the LICENSE.TXT file for more details.
 //
 
-#include "FromEdgeToCol.h"
-#include "DOF_Handles.h"
+// Inclusion from the standard library
 #include <cmath>
-#include <il/linear_algebra.h>
 #include <iostream>
 
+// Inclusion from Inside Loop library
+#include <il/linear_algebra.h>
 
-// Function that allow us to switch from end points (two values per node -> dof_dim = 2) to collocation points
+// Inclusion from the project
+#include "DOF_Handles.h"
+#include "FromEdgeToCol.h"
+
+namespace hfp {
+
+// Function which allow us to switch from end points (two values per node ->
+// dof_dim = 2) to collocation points
 // Remember: the elasticity is evaluated at collocation points
-// The collocation points are located in the reference element at location {-1/sqrt(2) , +1/sqrt(2)}
-// It is general ina sense that it includes both opening & shear
+// The collocation points are located in the reference unit element at location
+// {-1/sqrt(2) , +1/sqrt(2)}
+// It returns an array (vector) that includes EITHER shear (slip) OR opening at
+// collocation points
 
-il::Array<double> FromEdgeToCol(il::Array<double> &d_edge, il::Array2D<int> id, int Nelts, int dof_dim){
+il::Array<double> FromEdgeToCol(il::Array<double> &d_edge, const int Nelts,
+                                const int dof_dim) {
 
-    il::Array<double> Fetc{d_edge.size(),0.};
-    il::Array2D<double> ShapeFunction{4,4,.0};
+  // Inputs:
+  //  - d_edge -> vector that contains EITHER slip OR opening at nodal points
+  //  - Nelts -> number of elements
+  //  - dof_dim -> degrees of freedom per nodes
 
-    ShapeFunction(0,0) = (1+(1/sqrt(2)))/2;
-    ShapeFunction(0,1) = (1+(1/sqrt(2)))/2;
-    ShapeFunction(0,2) = (1-(1/sqrt(2)))/2;
-    ShapeFunction(0,3) = (1-(1/sqrt(2)))/2;
+  il::Array<double> Fetc{d_edge.size(), 0.};
+  il::Array2D<double> ShapeFunction{2, 2, .0};
 
-    ShapeFunction(1,0) = ShapeFunction(0,0);
-    ShapeFunction(1,1) = ShapeFunction(0,1);
-    ShapeFunction(1,2) = ShapeFunction(0,2);
-    ShapeFunction(1,3) = ShapeFunction(0,3);
+  ShapeFunction(0, 0) = (1 + (1 / sqrt(2))) / 2;
+  ShapeFunction(0, 1) = (1 - (1 / sqrt(2))) / 2;
 
-    ShapeFunction(2,0) = (1-(1/sqrt(2)))/2;
-    ShapeFunction(2,1) = (1-(1/sqrt(2)))/2;
-    ShapeFunction(2,2) = (1+(1/sqrt(2)))/2;
-    ShapeFunction(2,3) = (1+(1/sqrt(2)))/2;
+  ShapeFunction(1, 0) = (1 - (1 / sqrt(2))) / 2;
+  ShapeFunction(1, 1) = (1 + (1 / sqrt(2))) / 2;
 
-    ShapeFunction(3,0) = ShapeFunction(2,0);
-    ShapeFunction(3,1) = ShapeFunction(2,1);
-    ShapeFunction(3,2) = ShapeFunction(2,2);
-    ShapeFunction(3,3) = ShapeFunction(2,3);
+  il::Array2D<int> A{Nelts, 2, 0};
 
+  for (il::int_t k = 0, j; k < A.size(0); ++k) {
 
-    for (il::int_t i = 0, k =0; i < Nelts; ++i) {
+    j = k * dof_dim;
 
-        for (int j = 0; j < 2*dof_dim; j = j+2) {
+    for (il::int_t i = 0; i < A.size(1); ++i) {
 
-
-            Fetc[id(i,0)] = Fetc[id(i,0)] + ShapeFunction(0, j) * d_edge[id(i, j)];
-            Fetc[id(i,1)] = Fetc[id(i,1)] + ShapeFunction(1, j) * d_edge[id(i, j)];
-            Fetc[id(i,2)] = Fetc[id(i,2)] + ShapeFunction(2, j) * d_edge[id(i, j)];
-            Fetc[id(i,3)] = Fetc[id(i,3)] + ShapeFunction(3, j) * d_edge[id(i, j)];
-
-        }
-
+      A(k, i) = i + j;
     }
+  }
 
-    return Fetc;
+  for (il::int_t i = 0; i < Nelts; ++i) {
+
+    for (int j = 0; j < dof_dim; j = j + 2) {
+
+      Fetc[A(i, 0)] = Fetc[A(i, 0)] + ShapeFunction(0, j) * d_edge[A(i, j)];
+      Fetc[A(i, 1)] = Fetc[A(i, 1)] + ShapeFunction(1, j) * d_edge[A(i, j)];
+    }
+  }
+
+  return Fetc;
 };
+}
--- FromEdgeToCol.h
+++ FromEdgeToCol.h
@@ -2,16 +2,24 @@
 // This file is part of HFPx2D.
 //
 // Created by Federico Ciardo on 11.01.17.
-// Copyright (c) ECOLE POLYTECHNIQUE FEDERALE DE LAUSANNE, Switzerland, Geo-Energy Laboratory, 2016-2017.  All rights reserved.
-// See the LICENSE.TXT file for more details. 
+// Copyright (c) ECOLE POLYTECHNIQUE FEDERALE DE LAUSANNE, Switzerland,
+// Geo-Energy Laboratory, 2016-2017.  All rights reserved.
+// See the LICENSE.TXT file for more details.
 //
 
 #ifndef HFPX2D_FROMEDGETOCOL_H
 #define HFPX2D_FROMEDGETOCOL_H
 
-#include <il/linear_algebra.h>
+// Inclusion from standard library
 #include <cmath>
 
-il::Array<double > FromEdgeToCol(il::Array<double> &d_edge, il::Array2D<int> id, int Nelts, int dof_dim);
+// Inclusion from Inside Loop library
+#include <il/linear_algebra.h>
 
-#endif //HFPX2D_FROMEDGETOCOL_H
+namespace hfp {
+
+il::Array<double> FromEdgeToCol(il::Array<double> &d_edge, const int Nelts,
+                                const int dof_dim);
+}
+
+#endif // HFPX2D_FROMEDGETOCOL_H
