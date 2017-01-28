67cef72a445b7673389bf4ce7493179edde23aba
Federico Ciardo <federico.ciardo@epfl.ch>
2017-01-28 12:33:00 +0100

    ConductivitiesNewtonian -> general for either shear DD or normal DD

---
+++ ConductivitiesNewtonian.h
@@ -0,0 +1,24 @@
+//
+// This file is part of HFPx2D.
+//
+// Created by Federico Ciardo on 28.01.17.
+// Copyright (c) ECOLE POLYTECHNIQUE FEDERALE DE LAUSANNE, Switzerland,
+// Geo-Energy Laboratory, 2016-2017.  All rights reserved.
+// See the LICENSE.TXT file for more details.
+//
+
+#ifndef HFPX2D_CONDUCTIVITIESNEWTONIAN_H
+#define HFPX2D_CONDUCTIVITIESNEWTONIAN_H
+
+// Inclusion from standard library
+#include <cmath>
+
+// Inclusion from Inside Loop library
+#include <il/linear_algebra.h>
+
+il::Array<double> ConductivitiesNewtonian(const il::Array<double> &rho,
+                                          const il::Array<double> &vector,
+                                          const il::Array<double> EltSizes,
+                                          const double Visc);
+
+#endif // HFPX2D_CONDUCTIVITIESNEWTONIAN_H
