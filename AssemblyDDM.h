6df52e724f0e68226cda1960bc7162aa2a80ae5a
dongliuepfl <d.liu@epfl.com>
2017-02-08 10:43:03 +0100

    make functions being used in other files possible

--- AssemblyDDM.h
+++ AssemblyDDM.h
@@ -12,7 +12,7 @@
 
 #include <il/Array2D.h>
 #include "Mesh.h"
-
+//to make the functions being used in other files possible
 void set_submatrix(il::Array2D<double>& A, int i0, int i1, const il::StaticArray2D<double,2,4>& B);
 void take_submatrix(il::Array2D<double>& sub, int i0, int i1, int j0, int j1, const il::Array2D<double>& A);
 
